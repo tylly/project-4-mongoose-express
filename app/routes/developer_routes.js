d2f911c064511a2f7b4463ba423ce47d497c75d7
jamestylladams <jamestylladams@gmail.com>
2022-08-26 07:44:11 -0600

    shai back end

--- app/routes/developer_routes.js
+++ app/routes/developer_routes.js
@@ -52,7 +52,6 @@ router.post('/developers', requireToken, (req, res, next) => {
 		})
 		.catch(next)
 })
-
 //Add project to dev
 //Patch /developer/addProj/:projectId
 router.patch('/developers/addProj/:projectId', requireToken, removeBlanks, (req, res, next) => {
@@ -76,6 +75,20 @@ router.patch('/developers/addProj/:projectId', requireToken, removeBlanks, (req,
 	
 })
 
+// router.patch('/developers/addProj/:projectId/:devId', requireToken, removeBlanks, (req, res, next) => {
+// 	//delete req.body.developer.owner
+
+// 	Developer.findById(req.params.devId)
+// 		.then(handle404)
+// 		.then((developer) => {
+// 			requireOwnership(req, developer)
+// 			developer.projects.push(req.params.projectId)
+// 			return developer.save()
+// 		})
+// 		.then(() => res.sendStatus(204))
+// 		.catch(next)
+// })
+
 // UPDATE
 // PATCH /developers/5a7db6c74d55bc51bdf39793
 router.patch('/developers/:id', requireToken, removeBlanks, (req, res, next) => {
@@ -90,6 +103,7 @@ router.patch('/developers/:id', requireToken, removeBlanks, (req, res, next) => 
 		.then(() => res.sendStatus(204))
 		.catch(next)
 })
+
 
 // DESTROY
 // DELETE /developers/5a7db6c74d55bc51bdf39793
